e4ca92a4a90c9c6c248425342a23075ae4223e6f
Adrian Lambrese <itwarp_adrian.lambrese@iseatz.com>
2024-08-30 17:01:42 -0300

    Add drawer component

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "F I E B R E ®";
+export const SITE_DESCRIPTION = "Fiebre Design Studio - Buenos Aires Argentina";
