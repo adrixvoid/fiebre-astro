b2a58908c7a838235412788da6aa2c7abaf2cf57
Adrian Lambrese <adriantg@gmail.com>
2025-01-31 21:26:41 +1000

    Fix astro blog-portfolio config

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,18 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
-export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`
-		})),
-	});
-}
+// export async function GET(context) {
+// 	const posts = await getCollection('blog');
+// 	return rss({
+// 		title: SITE_TITLE,
+// 		description: SITE_DESCRIPTION,
+// 		site: context.site,
+// 		items: posts.map((post) => ({
+// 			...post.data,
+// 			link: `/blog/${post.slug}/`
+// 		})),
+// 	});
+// }
 
 export async function GET(context) {
 	const portfolio = await getCollection('portfolio');
@@ -23,8 +23,31 @@ export async function GET(context) {
 		site: context.site,
 		items: portfolio.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
-			categories: post.categories.reduce((acc, curr) => `${acc}, ${curr}`)
+			link: `/blog/${post.slug}/`
 		})),
 	});
 }
+
+
+// export async function GET(context) {
+// 	const posts = await getCollection('blog');
+// 	const portfolio = await getCollection('portfolio');
+
+// 	const portfolioItems = portfolio.map((item) => ({
+// 		...item.data,
+// 		link: `/blog/${item.slug}/`,
+// 		categories: item.categories ? item.categories.reduce((acc, curr) => `${acc}, ${curr}`) : []
+// 	}));
+
+// 	const blogItems = posts.map((post) => ({
+// 		...post.data,
+// 		link: `/blog/${post.slug}/`
+// 	}));
+
+// 	return rss({
+// 		title: SITE_TITLE,
+// 		description: SITE_DESCRIPTION,
+// 		site: context.site,
+// 		items: [...portfolioItems, ...blogItems]
+// 	});
+// }
